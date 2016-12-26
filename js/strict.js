22efc51247504dae6a77809f620cd4899be366cd
ruizhang <zhang.rui@yuejian.com>
2016-12-26 09:57:00 +0800

    update

---
+++ js/strict.js
@@ -0,0 +1,15 @@
+/**
+ * Created by ziyu on 2016/12/24.
+ */
+"use strict";
+var obj = null; //obj 复合类型数据
+var obj2 ; //复合类型 or 简单类型
+
+
+function  test() {
+	x =  10
+	console.log(x);
+
+}
+
+test();
